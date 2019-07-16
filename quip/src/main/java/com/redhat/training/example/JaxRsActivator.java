ef8c52b4d826d47e933ca9f1f1f799449eeb9af8
Richard Allred <richardmallred@gmail.com>
2019-07-16 08:45:30 -0400

    adding quip app

---
+++ quip/src/main/java/com/redhat/training/example/JaxRsActivator.java
@@ -0,0 +1,8 @@
+package com.redhat.training.example;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class JaxRsActivator extends Application {
+}
