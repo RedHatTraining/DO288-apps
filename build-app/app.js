266bf4b38c32946327a9ecf0885242724638ef01
Student User <student@workstation.lab.example.com>
2022-02-06 10:20:18 -0500

    2.0

--- build-app/app.js
+++ build-app/app.js
@@ -2,10 +2,10 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Simple app for the Building Applications Lab!\n');
+  res.send('Simple app for the Building Applications Lab 2.0!\n');
 });
 
 app.listen(8080, function () {
-  console.log('Simple app for the Building Applications Lab!');
+  console.log('Simple app for the Building Applications Lab 2.0!');
 });
 
