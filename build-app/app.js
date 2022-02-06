faf30f6800cb5fbff53b40bd63cddfc60b25a748
Student User <student@workstation.lab.example.com>
2022-02-06 10:33:53 -0500

    roll back

--- build-app/app.js
+++ build-app/app.js
@@ -2,10 +2,10 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Simple app for the Building Applications Lab 2.0!\n');
+  res.send('Simple app for the Building Applications Lab!\n');
 });
 
 app.listen(8080, function () {
-  console.log('Simple app for the Building Applications Lab 2.0!');
+  console.log('Simple app for the Building Applications Lab!');
 });
 
