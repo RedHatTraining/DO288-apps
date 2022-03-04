b45a63e1b41a628d1906afa3a81566e330e16192
Sreejith Anujan <sreejith@redhat.com>
2022-03-03 19:29:44 -0500

    Demo GitHub Webhook!

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,3 @@
 <?php
-print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "This is the GitHub Webhook "\n";
-print "This is the GitHub Webhook "\n";
+print "Hello, Earth! php version is " . PHP_VERSION . "\n";
 ?>
