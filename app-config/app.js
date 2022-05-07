57389874124663d0e7d104a68c8aa28863c2e457
IldarMukhametzianov <50869065+IldarMukhametzianov@users.noreply.github.com>
2022-05-07 18:15:09 +0300

    Update app.js

--- app-config/app.js
+++ app-config/app.js
@@ -3,13 +3,13 @@ var fs = require('fs')
 app = express();
 
 // read in the APP_MSG env var
-var msg = process.env.NPM_RUN;
+var msg = process.env.APP_MSG;
 
 var response;
 
 app.get('/', function (req, res) {
 
-    response = 'Value in the NPM_RUN env var is => ' + msg + '\n';
+    response = 'Value in the APP_MSG env var is => ' + msg + '\n';
 
     // Read in the secret file
     fs.readFile('/opt/app-root/secure/myapp.sec', 'utf8', function (secerr,secdata) {
