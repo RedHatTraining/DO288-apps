ce5f8e38f2ae479440d16b43a55b55b9831adbb7
Shravan Reddy <shravanreddy@Shravans-MacBook-Pro.local>
2022-06-23 12:25:32 -0400

    updated nodejs app

--- nodejs-helloworld/app.js
+++ nodejs-helloworld/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  res.send('Hello World, sample!\n');
 });
 
 app.listen(8080, function () {
