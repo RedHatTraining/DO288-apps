1edc5340316b7fef6a5a630454be2b09e893fb65
Alex Serna <asernabo@redhat.com>
2023-09-22 12:41:34 +0200

    fix oc-new-app.sh script in apps and solutions

--- apps/builds-applications/vertx-site/oc-new-app.sh
+++ apps/builds-applications/vertx-site/oc-new-app.sh
@@ -2,6 +2,7 @@
 
 oc new-app --name vertx-site \
   --build-env MAVEN_MIRROR_URL=http://nexus-infra.apps.ocp4.example.com/java \
+  --env JAVA_APP_JAR=vertx-site-1.0.0-SNAPSHOT-fat.jar \
   -i redhat-openjdk18-openshift:1.8 \
-  --context-dir builds-applications/apps/vertx-site \
+  --context-dir apps/builds-applications/vertx-site \
   https://git.ocp4.example.com/developer/DO288-apps
