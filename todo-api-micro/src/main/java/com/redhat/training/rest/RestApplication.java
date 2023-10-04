1c71237a99bfd4370d234008b1d61000eaefbcef
Alex Serna <asernabo@redhat.com>
2023-10-04 14:41:23 +0200

    remove old applications

--- todo-api-micro/src/main/java/com/redhat/training/rest/RestApplication.java
+++
@@ -1,8 +0,0 @@
-package com.redhat.training.rest;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/todo/api")
-public class RestApplication extends Application {
-}
--- todo-api-micro/src/main/resources/sql/load.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `Item` (`id`,`description`,`done`) VALUES (1,'Pick up newspaper', 0);
-INSERT INTO `Item` (`id`,`description`,`done`) VALUES (2,'Buy groceries', 1);
